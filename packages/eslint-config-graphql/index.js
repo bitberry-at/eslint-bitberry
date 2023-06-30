1f0b8bb3d4c0fa6a287b6f76b218bf74850c5cd9
aydreas <andreas.sch4@gmail.com>
2023-06-30 14:03:24 +0200

    updated graphql package

--- packages/eslint-config-graphql/index.js
+++ packages/eslint-config-graphql/index.js
@@ -1,22 +1,15 @@
 module.exports = {
     extends: [
-        'plugin:graphql/'
+        'plugin:@graphql-eslint/schema-recommended',
+        'plugin:@graphql-eslint/operations-recommended'
     ],
     rules: {
-        'vue/attributes-order': 'warn',
-        'vue/order-in-components': 'warn',
-        'vue/html-indent': ['warn', 4]
+        '@graphql-eslint/known-type-names': 'error'
     },
     overrides: [
         {
             files: ["*.graphql"],
-            extends: [
-                "plugin:@graphql-eslint/schema-recommended",
-                "plugin:@graphql-eslint/operations-recommended"
-            ],
-            rules: {
-                "@graphql-eslint/known-type-names": "error"
-            }
+            parser: "@graphql-eslint/eslint-plugin"
         }
     ]
 };
