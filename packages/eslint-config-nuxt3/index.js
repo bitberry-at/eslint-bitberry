3432a6999ee6e6f2fd7c725c63122dbcbd3a5a85
aydreas <andreas.sch4@gmail.com>
2023-07-07 12:40:59 +0200

    feat: updated nuxt3 rules

--- packages/eslint-config-nuxt3/index.js
+++ packages/eslint-config-nuxt3/index.js
@@ -3,6 +3,10 @@ module.exports = {
         'plugin:nuxt/recommended',
         'plugin:vue/vue3-recommended'
     ],
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.vue']
+    },
     rules: {
         'vue/attributes-order': 'warn',
         'vue/order-in-components': 'warn',
@@ -12,6 +16,21 @@ module.exports = {
         {
             files: ['*.vue'],
             parser: 'vue-eslint-parser'
+        },
+        {
+            files: [
+                // These pages are not used directly by users, so they can have one-word names.
+                '**/pages/**/*.{js,ts,jsx,tsx,vue}',
+                '**/layouts/**/*.{js,ts,jsx,tsx,vue}',
+                '**/app.{js,ts,jsx,tsx,vue}',
+                '**/error.{js,ts,jsx,tsx,vue}'
+            ],
+            rules: { 'vue/multi-word-component-names': 'off' }
+        },
+        {
+            // Pages and layouts are required to have a single root element if transitions are enabled.
+            files: ['**/pages/**/*.{js,ts,jsx,tsx,vue}', '**/layouts/**/*.{js,ts,jsx,tsx,vue}'],
+            rules: { 'vue/no-multiple-template-root': 'error' }
         }
     ]
 };
